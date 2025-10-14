751f157e85da3be46b8ebf6504f6c67b1456b29d
viczinoviev <106315428+viczinoviev@users.noreply.github.com>
2025-10-14 01:13:12 -0400

    Utilize gametime to handle effects.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,14 +5,33 @@ public class Enemy : MonoBehaviour
     private SpriteRenderer sr;
     private float damageTime = 0.1f;
 
+    [SerializeField]private float redColorDuration = 1;
+
+    public float currentTimeInGame;
+    public float lastTimeDamaged;
+
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
     }
+
+    private void Update()
+    {
+        currentTimeInGame = Time.time;
+
+        if (currentTimeInGame > lastTimeDamaged + redColorDuration)
+        {
+            if (sr.color != Color.white)
+            {
+                TurnWhite();
+            }
+        }
+    }
+
     public void TakeDamage()
     {
         sr.color = Color.red;
-        Invoke(nameof(TurnWhite), damageTime);
+        lastTimeDamaged = Time.time;
     }
 
     private void TurnWhite()
